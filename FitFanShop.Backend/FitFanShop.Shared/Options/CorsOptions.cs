cd63b870bdc4ade4ee34267fa4062be9779e157d
Harun Melić <harun1melic6@gmail.com>
2025-12-17 02:16:42 +0100

    #25 - Small Project Structure Update

---
+++ FitFanShop.Backend/FitFanShop.Shared/Options/CorsOptions.cs
@@ -0,0 +1,6 @@
+namespace FitFanShop.Shared.Options;
+
+public class CorsOptions
+{
+    public string[] AllowedOrigins { get; set; } = [];
+}
---
+++ FitFanShop.Backend/FitFanShop.Shared/Options/LocalizationOptions.cs
@@ -0,0 +1,7 @@
+namespace FitFanShop.Shared.Options;
+
+public class LocalizationOptions
+{
+    public string DefaultCulture { get; set; } = "bs-BA";
+    public string[] SupportedCultures { get; set; } = [];
+}
