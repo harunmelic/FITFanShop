652aa4bd743fe3237de148331d44d634965d4c56
Harun Melić <harun1melic6@gmail.com>
2026-02-03 20:21:24 +0100

    56 - Forgot Paassword

---
+++ FitFanShop.Backend/FitFanShop.Frontend/src/app/app-routing-module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/home/home-module').then(m => m.HomeModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth-module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'catalog',
+    loadChildren: () =>
+      import('./modules/catalog/catalog-module').then(m => m.CatalogModule)
+  },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
